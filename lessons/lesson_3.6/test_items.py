4df9c4f2b20330b86dc63de177b82d3bc66129f0
aminzin <jiganoaldog@gmail.com>
2020-03-21 11:31:19 +0300

    rename method

--- lessons/lesson_3.6/test_items.py
+++ lessons/lesson_3.6/test_items.py
@@ -5,7 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 # вызываем фикстуру в тесте, передав ее как параметр
-def test_guest_should_see_login_link(browser):
+def test_basket_button(browser):
     browser.get(link)
     # time.sleep(5)  # использовалось для проверки решения (2ой пункт)
     browser.find_element_by_class_name("btn-add-to-basket")
