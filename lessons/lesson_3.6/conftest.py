2904e1533f26c50c15f93b06f9f8f64d030553be
aminzin <jiganoaldog@gmail.com>
2020-03-21 11:27:22 +0300

    edited conf and items_test

--- lessons/lesson_3.6/conftest.py
+++ lessons/lesson_3.6/conftest.py
@@ -14,7 +14,7 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart chrome browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
--- lessons/lesson_3.6/test_items.py
+++ lessons/lesson_3.6/test_items.py
@@ -1,13 +1,13 @@
-import pytest
 import time
-from selenium import webdriver
+
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-class TestMain():
-    # вызываем фикстуру в тесте, передав ее как параметр
-    def test_guest_should_see_login_link(self, browser):
-        browser.get(link)
-        time.sleep(30)
-        browser.find_element_by_class_name("btn-add-to-basket")
+# вызываем фикстуру в тесте, передав ее как параметр
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    # time.sleep(5)  # использовалось для проверки решения (2ой пункт)
+    browser.find_element_by_class_name("btn-add-to-basket")
+    # проверка длины строки текста на кнопке
+    assert len(browser.find_element_by_class_name("btn-add-to-basket").text) > 0
